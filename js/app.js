db02b3ab86773b1f547d10318d162b7f5592d390
Joseph Campuzano <campuzanojoseph@gmail.com>
2016-08-21 02:59:27 -0700

    start/stopping(pausing) the game is implemented. TODO: implement resume, and reset

--- js/app.js
+++ js/app.js
@@ -1,38 +1,31 @@
-// Option elements (time/play)
-var min = document.getElementById('minutes');
-var seconds = document.getElementById('seconds');
-var setMinutes = document.getElementById('set-minutes');
-var setSeconds = document.getElementById('set-seconds');
 var form = $('#set-timer');
+var play = $('#play');
+var pause = $('#pause');
+var reset = $('#reset');
 
-// Player Actions
-var rock = document.getElementById('rock');
-var paper = document.getElementById('paper');
-var scissors = document.getElementById('scissors');
-
-// Scoreboard Elements
-var wins = document.getElementById('win');
-var losses = document.getElementById('loss');
-var ties = document.getElementById('tie');
-
-
-var Timer = function() {
-	var start = $('#set-timer'),
-	seconds = document.getElementById('seconds'),
+// Timer object constructor, used to manage the time/stopwatch for the game
+var Timer = function(endCallback) {
+	var seconds = document.getElementById('seconds'),
 	minutes = document.getElementById('minutes'),
 	setMinutes = document.getElementById('set-minutes'),
 	setSeconds = document.getElementById('set-seconds'),
+	endCallback = endCallback || null,
 	interval;
 
+	// Set the timer's initial values based on the current inputs
 	function setTimer() {
 		clearInterval(interval);
 		if (!setSeconds.value) {
+			secondsVal = 0;
 			seconds.innerHTML = "00";
 		} else if (setSeconds.value.length < 2) {
+			seconds.Val
 			seconds.innerHTML = "0" + setSeconds.value;
 		} else if (setSeconds.value > 59) {
-			seconds.innerHTML = setSeconds.value % 60;
-			setMinutes.value = parseInt(setMinutes.value) + Math.floor(setSeconds.value / 60);
+			seconds.innerHTML = setSeconds.value > 69 ? setSeconds.value % 60 : "0" + setSeconds.value;
+			setMinutes.value = setMinutes.value ? parseInt(setMinutes.value) + Math.floor(setSeconds.value / 60) : Math.floor(setSeconds.value/60);
+			console.log(setMinutes.value);
+			setSeconds.value = setSeconds.value % 60;
 		} else {
 			seconds.innerHTML = setSeconds.value;
 		}
@@ -46,10 +39,16 @@ var Timer = function() {
 		}
 	}	
 
+	// Start running the timer (updates every second);
 	function runTimer() {
 		interval = setInterval(updateSeconds, 1000);
 	}
 
+	function stopTimer() {
+		clearInterval(interval);
+	}
+
+	// Updates the second counter for the timer, if seconds reaches 0 then reset to 59 and decrement the minute count.
 	function updateSeconds() {
 		newVal = seconds.innerHTML - 1;
 		if (newVal > 9) {
@@ -62,6 +61,7 @@ var Timer = function() {
 		}
 	}
 
+	// Updates the minute counter for the timer, if minutes attempts set minutes below 0, then set the seconds/minutes to 0 and stop the timer
 	function updateMinutes() {
 		newVal = minutes.innerHTML - 1;
 		if (newVal > 9) {
@@ -71,79 +71,124 @@ var Timer = function() {
 		} else if (newVal < 0) {
 			clearInterval(interval);
 			seconds.innerHTML = minutes.innerHTML = "00";
+			if (endCallback && typeof endCallback == "function") endCallback();
 		}
 	}
+
+	function setEndCallback(callback) {
+		endCallback = callback;
+	}
+	//public methods, set the timer, and run/start the timer
 	this.setTimer = setTimer;	
 	this.runTimer = runTimer;
+	this.stopTimer = stopTimer;
+	this.setEndCallback = setEndCallback;
 }
 
-var stopwatch = new Timer();
+var Game = function() {
+	var wins = document.getElementById('win'),
+		losses = document.getElementById('loss'),
+		ties = document.getElementById('tie'),	
+		playerAction = document.getElementById('player-action'),
+		botAction = document.getElementById('bot-action'),
+		possibleActions = document.querySelectorAll('li'), // Rock, Paper, Scissors
+		boundActions = [];
+		stopwatch =  new Timer(findWinner);
+
+	function randomAction() {
+		var actions = {
+			0: 'rock',
+			1: 'paper',
+			2: 'scissors'
+		};
+		var action = Math.floor(Math.random() * 3);
+		return actions[action];
+	}
+
+	function takeAction(action) {
+		var playerMove = this.innerHTML.toLowerCase();
+		var botMove = randomAction();
+		updateScore(playerMove, botMove);
+		playerAction.src = "img/" + playerMove + ".jpg";
+		botAction.src = "img/" + botMove + ".jpg";
+	}
+
+	function enableActions() {
+		for (var i = 0; i < possibleActions.length; i++) {
+			var handler = takeAction.bind(possibleActions[i]);
+			boundActions.push(handler);
+			possibleActions[i].addEventListener('click', handler)
+		}
+		stopwatch.setTimer();
+		stopwatch.runTimer(findWinner);
+		console.log(this);
+	}
+
+	function disableActions() {
+		stopwatch.stopTimer();
+		console.log("what");
+		for (var i = 0; i < possibleActions.length; i++) {
+			possibleActions[i].removeEventListener('click', boundActions[i]);
+		}
+		boundActions = [];
+	}
+
+	function findWinner() {
+		console.log('SOMEONE WON THE GAME...');
+	}
+
+	function updateScore() {
+		var args = Array.prototype.slice.call(arguments);
+		var winValue = parseInt(wins.innerHTML),
+		    lossValue = parseInt(losses.innerHTML),
+		    tieValue = parseInt(ties.innerHTML);
+
+		// if no arguments, or the arguments are equal, then reset the scoreboard or add 1 to ties
+		if (!args.length) {
+			wins.innerHTML = "0";
+			losses.innerHTML = "0";
+			ties.innerHTML = "0";
+			return;
+		} else if (args[0] == args[1]) {
+			ties.innerHTML = tieValue + 1;
+			return;
+		}
+
+		// Otherwise check arguments for win state
+		switch (args[0]) {
+			case 'rock':
+				args[1] == 'scissors' ? wins.innerHTML = winValue + 1 : losses.innerHTML = lossValue + 1;
+				break;
+			case 'paper':
+				args[1] == 'rock' ? wins.innerHTML = winValue + 1 : losses.innerHTML = lossValue + 1;
+				break;
+			case 'scissors':
+				args[1] == 'paper' ? wins.innerHTML = winValue + 1 : losses.innerHTML = lossValue + 1;
+				break;
+		}
+	}
+
+	this.enableActions = enableActions;
+	this.disableActions = disableActions;
+}
+
+var roShamBo = new Game();
 
 form.on('submit', function(e) {
 	e.preventDefault();
-	console.log(randomAction());
-	stopwatch.setTimer();
-	stopwatch.runTimer();
+	//stopwatch.setTimer();
+	//stopwatch.runTimer();
+	roShamBo.enableActions();
 });
 
-/* Let the bot choose a random action */
-function randomAction() {
-	actions = {
-		0: 'rock',
-		1: 'paper',
-		2: 'scissors'
-	};
-	var randomAction = Math.floor(Math.random() * 3);
-	return actions[randomAction];
-}
+play.on('click', function(e) {
+	roShamBo.enableActions();
+});
 
-/* Function to update score, if passed no arguments, will reset the score back to default (0,0,0);
- * otherwise, it takes two arguments, the players action, and bots action. Decides the winner, and 
- * updates the score accordingly
- */
-function updateScore() {
-	var args = Array.prototype.slice.call(arguments);
-	var winValue = parseInt(wins.innerHTML),
-	    lossValue = parseInt(losses.innerHTML),
-	    tieValue = parseInt(ties.innerHTML);
+pause.on('click', function(e) {
+	roShamBo.disableActions();
+})
 
-	// if no arguments, or the arguments are equal, then reset the scoreboard or add 1 to ties
-	if (!args.length) {
-		wins.innerHTML = "0";
-		losses.innerHTML = "0";
-		ties.innerHTML = "0";
-		return;
-	} else if (args[0] == args[1]) {
-		ties.innerHTML = tieValue + 1;
-		return;
-	}
-
-	// Otherwise check arguments for win state
-	switch (args[0]) {
-		case 'rock':
-			args[1] == 'scissors' ? wins.innerHTML = winValue + 1 : losses.innerHTML = lossValue + 1;
-			break;
-		case 'paper':
-			args[1] == 'rock' ? wins.innerHTML = winValue + 1 : losses.innerHTML = lossValue + 1;
-			break;
-		case 'scissors':
-			args[1] == 'paper' ? wins.innerHTML = winValue + 1 : losses.innerHTML = lossValue + 1;
-			break;
-	}
-}
-
-function roShamBo() {
-	var els = document.querySelectorAll('li');
-	for (var i = 0; i < els.length; i++) {
-		els[i].addEventListener('click', function() {
-			var playerAction = this.innerHTML.toLowerCase();
-			var botAction = randomAction();
-			updateScore(playerAction, botAction);
-			document.getElementById('player-action').src = "img/" + playerAction + ".jpg";
-			document.getElementById('bot-action').src = "img/" + botAction + ".jpg";
-		});
-	}
-}
-
-roShamBo();
-
+reset.on('click', function(e) {
+	roShamBo.disableActions();
+})
